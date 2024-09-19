9b0c6adb4c1cd5afc7ffcf2f4722d6399fa4c6e1
Kumaaaaaaaaaaaakuaaaaa <161785768+Kumaaaaaaaaaaaakuaaaaa@users.noreply.github.com>
2024-09-19 22:56:34 +0800

    Update oakMe.js

--- HW1/oakMe.js
+++ HW1/oakMe.js
@@ -6,10 +6,59 @@ app.use((ctx) => {
   // ctx.response.status = 404
   console.log('url=', ctx.request.url)
   let pathname = ctx.request.url.pathname
-  if (pathname == '/name') {
-    ctx.response.body = '吳畇蓁'
+  if (pathname == '/') {
+    ctx.response.body =
+     `<html>
+<head>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+div {
+ width: 10px;
+  height: 100vh;
+  background: white;
+  transition: width 1s;
+}
+
+.expanded {
+  width: 100px;
+}
+</style>
+</head>
+<body>
+
+
+<h1>クマの自己紹介 </h1>
+
+<p>打開連結將會更多資訊</p>
+<button onclick="button()">＼＼＼</button>
+<div id="myDiv"> 
+  <ol>
+<li><a href="/name">なまえわ？</a></li>
+<li><a href="/age">何歳ですが？</a></li>
+<li><a href="/gender">性別は何ですか？</a></li>
+</ol>
+</div>
+<script>
+function button() {
+  var element = document.getElementById("myDiv");
+  element.classList.toggle("expanded");
+}
+</script>
+
+</body>
+</html>
+`
+  } else if (pathname == '/name') {
+    ctx.response.body = '卡皮巴拉·畇蓁'
+  } else if (pathname == '/age') {
+    ctx.response.body = '350'
+  } else if (pathname == '/gender') {
+    ctx.response.body = 'normal'
   } else {
-    
+
   }
   // ctx.response.body = 'Not Found!'
 });
